e0f1a09b16671461a29d269c79ebf44e9d273b79
sshah <sshah402@gatech.edu>
2018-09-07 21:06:04 -0400

    fixed

--- load_data.py
+++ load_data.py
@@ -3,10 +3,11 @@ import pandas as pd
 import numpy as np
 
 def load_key_features(s_file):
-    s_file = os.path.join(os.getcwd(), 'data/' + s_file)
+    s_file = os.path.join(os.getcwd(), 'data', s_file)
     df_data = pd.read_csv(s_file)
     return df_data
 
 
 if __name__=="__main__":
-    load_key_features('train.csv')
+    load_key_features('training.csv')
+    # np.reshape(np.fromstring(df_data.iloc[i_image_id][0], dtype=int,sep=' '), (96, 96))
