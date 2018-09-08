1668daf2598b28554ab0ddbd64c92afd36ac953a
Sahaj Bhatt <sahajb97@gmail.com>
2018-09-07 23:38:43 -0400

    Created basic webapp layout

---
+++ webapp/app/__init__.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from datetime import datetime
+import json
+
+app = Flask(__name__) # create the application instance
+
+@app.route('/')
+def home():
+
+    
+	return render_template('home.html')
+
